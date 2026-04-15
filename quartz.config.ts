6509b20b90951b4ad51c0e6fa6aa38f42266e15b
Hyun <77914771+Small-Piece@users.noreply.github.com>
2026-04-15 17:14:43 +0900

    fix: update baseUrl again

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "small-piece.github.io/unreal-advanced-notes",
+    baseUrl: "Small-Piece.github.io/unreal-advanced-notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
